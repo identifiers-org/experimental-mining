c96d8b723a2802d13ffccaa11ac5bba9a79ede27
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-02 03:49:06 +0000

    debug code removed

--- samples/metadata_reporter.py
+++ samples/metadata_reporter.py
@@ -154,9 +154,6 @@ for pid_entry in resolver_dump:
 print("Test URLs description\n{}".format(metadata_report.ResourceTestUrl.describe()))
 print("Sample content\n{}".format(metadata_report.head()))
 
-# DEBUG --- REMOVE ---
-sys.exit(0)
-
 # Build the report
 # Parallel call to metadata service, this is a lot slower, good enough as proof of concept
 #metadata_requests = {index: threading.Thread(target=get_metadata_for_url(metadata_report.loc[index].ResourceTestUrl)) for index in range(metadata_report.shape[0]) if metadata_report.loc[index].ResourceTestUrl}
